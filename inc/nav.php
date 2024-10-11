1500a56ad28ae6c85fe2d99cdf93086c17d71721
Ron <91494083+fsadesign@users.noreply.github.com>
2024-10-11 15:58:38 -0400

    new portfolio

---
+++ inc/nav.php
@@ -0,0 +1,11 @@
+  <nav class="mobNav">
+    <a class="animateTo" href="#businesses">Businesses</a> <a class="animateTo" href="#investors">Investors</a> <a class="animateTo" href="#faq">FAQ</a> <a class="open-popup-link" href="#contactModal">Contact</a> <a class="open-popup-link" href="#loginModal">Log in</a>
+  </nav>
+  <header>
+    <div class="inner">
+      <a class="toggle-panel fa fa-bars"></a> <a class="animateTo" href="#panelToggle"><img class="logo" src="img/vrfid-logo.svg"></a>
+      <nav>
+        <a class="animateTo" href="#businesses">Businesses</a> <a class="animateTo" href="#investors">Investors</a> <a class="animateTo" href="#faq">FAQ</a> <a class="open-popup-link" href="#contactModal">Contact</a> <a class="open-popup-link" href="#loginModal">Log in</a>
+      </nav>
+    </div>
+  </header>
